28788805b0b09a831c80c9df6e7d3d15b9152d04
Lydia Morscher <lydia.morscher@web.de>
2022-03-11 14:38:38 +0100

    wrote init comment

---
+++ SimCode.py
@@ -0,0 +1,10 @@
+#Initialization 
+import ParticleClass as par
+
+def init(L, rho, eta, varTB = 0.03):
+    """initialized the particles and the lattice
+            Args:   L - size of the square shaped cell
+                    rho - density of the system, determines the particle number N (rho = N/L**2)
+                    eta - noise of the system
+                    varTB - absolute velocity at the beginning (default value of the paper 0,03)"""
+    pass
