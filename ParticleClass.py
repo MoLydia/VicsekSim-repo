9c73ca8fd844f233b076f26a6da41a6bbfcb2ff3
Lydia Morscher <lydia.morscher@web.de>
2022-03-18 21:12:34 +0100

    added x setter and x update

--- ParticleClass.py
+++ ParticleClass.py
@@ -14,12 +14,25 @@ class Particle():
                         x - (array) position
                         v - (array) velocity
                         theta - (array) direction"""
-
+        
+        self.L = L
         self.x = np.array((np.random.random() * L, np.random.random() * L))
         self.varT = varT
         theta = np.array((np.random.random() * L, np.random.random() * L))
         self.theta = theta / np.linalg.norm(theta)
         self.v = self.theta * self.varT
+
+    @property
+    def x(self):
+        """Getter of x"""
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if value[0] > self.L or value[1] > self.L :
+            raise ValueError("The Particle is out of the cell")
+        else:
+            self._x = value
 
     @property
     def theta(self):
@@ -34,6 +47,13 @@ class Particle():
         if np.linalg.norm(value) == 1:
             self._theta = value
         else: raise ValueError("The absolute value of theta has to be 1")
+
+    def updateX(self, ts):
+        """updates the position of a particle
+            Arg.:   ts - (double) timestep of the update"""
+        self.x = self.x + self.v * ts
+
+        
 
 
 
