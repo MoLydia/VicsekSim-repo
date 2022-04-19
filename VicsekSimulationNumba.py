7f2761903f886cef0529f6ae0235af2ae552d8a4
Lydia Morscher <lydia.morscher@web.de>
2022-04-19 13:17:05 +0200

    Updated the Numba file
    Simulation works; added comments; Same error as in the VicsekSimulation.py: visible in numbaError1.gif; error probably in numbaUpdateTheta-periodic boundary conditions

--- VicsekSimulationNumba.py
+++ VicsekSimulationNumba.py
@@ -1,32 +1,39 @@
-"""Vicsek Simulation optimized by numba"""
+"""Vicsek Simulation optimized by numbas @jitn
+    creates a simulation of phase transition in a system of self-driven particles after Tamas Vicsek
+    
+    In the following '_i' labels the variable at the timestep i and '_ip1' labels the variable at the timestep i plus 1"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from numba import jit
+from numba import njit
 
-@jit
+@njit
 def numbaUpdateX(N, x_i, v_i, L):
-    """Updates the positions of a particles; timestep is set to 1; 1. step of numbaUpdate
+    """Updates the positions of a particles; timestep is set to 1; 1 step of numbaUpdate
         Args.:  N (int) - Number of particles
-                x_i (array 2xN) - positions of the particles at timestep i
-                v (array 2xN) - calculated velocitys of the particles
+                x_i (array Nx2) - positions of the particles at timestep i
+                v_i (array Nx2) - calculated velocitys of the particles
                 L (double) - length of the box
-        Return: x_ip1 (array 2xN) - positions of the partiles at timestep i+1"""
+        Return: x_ip1 (array Nx2) - positions of the partiles at timestep i+1"""
+    #Integration
     x_ip1 = x_i + v_i
+
+    #Putting the particle back in the box
     for i in range(N):
         for j in range(2):
-            while x_ip1[i,j] > L:
-                x_ip1[i,j] =  x_ip1[i,j] - L
-            while x_ip1[i,j] < 0:
+            if x_ip1[i,j] > L:
+                x_ip1[i,j] = x_ip1[i,j] - L
+            if x_ip1[i,j] < 0:
                 x_ip1[i,j] = x_ip1[i,j] + L
+
     return x_ip1
 
-@jit
+@njit
 def numbaUpdateTheta(x_i, N, theta_i, L):
-    """Updates the angles of the particles without noise; update radius is set to 1; 2. step of numbaUpdate
-        Args.:  N  (int) - number of particles
-                x_i (array 2xN) - positions of the particles at timestep i
+    """Updates the angles of the particles without noise; update radius is set to 1; 2 step of numbaUpdate
+        Args.:  x_i (array 2xN) - positions of the particles at timestep i
+                N  (int) - number of particles
                 theta_i (array 1xN) - angles of the particles at the timestep i
                 L (double) - length of the box
         Return: theta_ip1 (array 1xN) - angles of the particles at the timestep i+1 without noise"""
@@ -38,29 +45,36 @@ def numbaUpdateTheta(x_i, N, theta_i, L):
             xr = x_i[i] - x_i[j]
             #periodic boundary conditions
             xr = xr - L/2 * np.array((int(xr[0]/(L/2)),int(xr[1]/(L/2))))
+            #radius of influence set to 1
             if np.linalg.norm(xr) <= 1:
                 thetaSin.append(np.sin(theta_i[j]))
                 thetaCos.append(np.cos(theta_i[j]))
-        theta_ip1[i] = np.arctan2(np.mean(thetaSin), np.mean(thetaCos))
+        #claculation of the new angle for every particle
+        thetaSin = np.mean(np.array(thetaSin))
+        thetaCos = np.mean(np.array(thetaCos))
+        theta_ip1[i] = np.arctan2(thetaSin, thetaCos)
 
     return theta_ip1
 
-@jit
+@njit
 def numbaUpdateV(N, theta_ip1, varT, eta):
-    """Updates the velocities of the particles; 3. step of numbaUpdate
+    """Updates the velocities of the particles; 3 step of numbaUpdate
         Args.:  N (int) - number of particles
                 theta_ip1 (array 1xN) - angles of the particles at timestep i+1 without noise
                 varT (double) - given absolute velocity of each particle
-                eta (double) - noise of the simulation
+                eta (double) - noise added to theta
         Return: v_ip1 (array 2xN) - velocities of particles at timestep i+1
                 theta_ip1 (array 2xN) - angles of the particles at timestep i+1 with noise"""
-    v_ip1 = np.zeros(N)
+    v_ip1 = np.zeros((N,2))
     for i in range(N):
-        theta_ip1[i] += np.random.uniform(-eta/2,eta/2)
-        v_ip1 = np.array((np.cos(theta_ip1[i]), np.sin(theta_ip1[i]))) * varT
+        #adding noise to the calculated angle theta for the timestep i+1
+        theta_ip1[i] = theta_ip1[i] + np.random.uniform(-eta/2,eta/2)
+        #calculating the vector v out of the angle with given absolute velocity varT
+        v_ip1[i] = np.array((np.cos(theta_ip1[i]), np.sin(theta_ip1[i]))) * varT
+
     return v_ip1, theta_ip1
 
-@jit
+@njit
 def numbaUpdate(N, varT, L, x_i, v_i, theta_i, eta):
     """Calculates the simluation after one timestep (i+1) with the following steps:
 
@@ -71,15 +85,16 @@ def numbaUpdate(N, varT, L, x_i, v_i, theta_i, eta):
         Args.:  N (int) - number of the particles 
                 varT (double) - given absolute velocity of each particle
                 L (double) - length of the box
-                x_i (array 2xN) - positions of the particles at timestep i
-                v_i (array 2xN) - velocities of the particles at timestep i
-                theta_i (array 2xN) - angles of the particles at timestep i
-        Return: x_ip1 (array 2xN) - positions of the particles at timestep i+1
-                theta_ip1 (array 2xN) - angles of the particles at timestep i+1
-                v_ip1 (array 2xN) - velocities of the particles at timestep i+1
+                x_i (array Nx2) - positions of the particles at timestep i
+                v_i (array Nx2) - velocities of the particles at timestep i
+                theta_i (array Nx2) - angles of the particles at timestep i
+                eta (double) - noise added to angle theta
+        Return: x_ip1 (array Nx2) - positions of the particles at timestep i+1
+                theta_ip1 (array Nx2) - angles of the particles at timestep i+1
+                v_ip1 (array Nx2) - velocities of the particles at timestep i+1
                 """
     #Step 1
-    x_ip1 = numbaUpdateX(x_i, N, v_i, L)
+    x_ip1 = numbaUpdateX(N, x_i, v_i, L)
     #Step 2
     theta_ip1 = numbaUpdateTheta(x_i, N, theta_i, L)
     #Step 3 
@@ -87,46 +102,65 @@ def numbaUpdate(N, varT, L, x_i, v_i, theta_i, eta):
 
     return x_ip1, theta_ip1, v_ip1
 
-def Funcupdate(ax1, N, varT, L, x_i, v_i, theta_i, eta, i):
-    """The method FuncAnimation performs for each frame; plots the resent positions of the particles
-        Args.:  ax1 (plt.axis) - axis of the plot 
-                L (double) - length of the box
-                (i (int) - frame number; used in animation.FuncAnimation) """
-    #Resetting the axes
-    ax1.clear()
-    ax1.set_xlim([0,L])
-    ax1.set_ylim([0,L])
 
-    #One timestep of the simulation
-    x_ip1, theta_ip1, v_ip1 = numbaUpdate(N, varT, L, x_i, v_i, theta_i, eta)
-    
-    #Plot
-    ax1.quiver(x_ip1[:,[0]],x_ip1[:,[1]],v_ip1[:,[0]],v_ip1[:,[1]] )
-    
-    
-def main(N, varT, L, animate, eta, frameMax, name):
-    """Simulates the phase transition of active particles after the Vicsek model; main method"""
+class Vicsek:
 
-    #Initialization
-    x_i = np.zeros((2,N))
-    theta_i = np.zeros((2,N))
-    v_i = np.zeros((2,N))
-    for i in range(N):
-        x_i[i] = np.array((np.random.random() * L, np.random.random() * L))
-        theta_i[i] = 2 * np.pi * np.random.random()
-        v_i[i] = np.array((np.cos(theta_i), np.sin(theta_i))) * varT
+    def __init__(self, N, L, eta, varT = 0.03):
+        """Initialization of Vicsek
+            Args.:  N (int) - number of the particles 
+                    L (double) - length of the box
+                    eta (double) - noise added to angle theta
+                    varT (double) - given absolute velocity of each particle; set to varT = 0.03
+            Attr.:  N (as above)
+                    L (as above)
+                    varT (as above)
+                    x_i (array Nx2) - positions of the particles at timestep i
+                    v_i (array Nx2) - velocities of the particles at timestep i
+                    theta_i (array Nx2) - angles of the particles at timestep i
+                    fig (plt figure) - figure of the plot
+                    ax1 (plt axis) - axis of the plot
+                    """
+        self.N = N
+        self.L = L
+        self.eta = eta
+        self.varT = varT
+        self.x_i = np.zeros((N,2))
+        self.theta_i = np.zeros(N)
+        self.v_i = np.zeros((N,2))
+        for i in range(N):
+            #init of x with random positions
+            self.x_i[i] = np.array((np.random.random() * L, np.random.random() * L))
+            #init of theta with random angles
+            self.theta_i[i] = 2 * np.pi * np.random.random()
+            #calculating v out of the angle and the absolute velocity varT
+            self.v_i[i] = np.array((np.cos(self.theta_i[i]), np.sin(self.theta_i[i]))) * varT
 
-    #Plot of the Initialization
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1,1,1)
-    ax1.quiver(x_i[:,[0]],x_i[:,[1]],v_i[:,[0]],v_i[:,[1]])
-    ax1.set_xlim([0,L])
-    ax1.set_ylim([0,L])
-    plt.show()
+        #Plot of the Initialization
+        self.fig = plt.figure()
+        self.ax1 = self.fig.add_subplot(1,1,1)
+        self.ax1.quiver(self.x_i[:,[0]],self.x_i[:,[1]],self.v_i[:,[0]],self.v_i[:,[1]])
+        self.ax1.set_xlim([0,L])
+        self.ax1.set_ylim([0,L])
 
-    #Animation of the Simulation if animate == True
-    if animate:
-        simu = animation.FuncAnimation(fig, Funcupdate, frames = np.arange(0,frameMax), interval = 25)
+    def funcUpdate(self, i):
+        """The method FuncAnimation performed for each frame; plots the resent positions of the particles
+            Args.:  (i (int) - frame number; used in animation.FuncAnimation) """
+        #Resetting the axes
+        self.ax1.clear()
+        self.ax1.set_xlim([0,self.L])
+        self.ax1.set_ylim([0,self.L])
+
+        #One timestep of the simulation
+        x_ip1, theta_ip1, v_ip1 = numbaUpdate(self.N, self.varT, self.L, self.x_i, self.v_i, self.theta_i, self.eta)
+        #print(np.linalg.norm(self.x_i-x_ip1))
+        self.x_i, self.theta_i, self.v_i = x_ip1, theta_ip1, v_ip1
+        #Plot
+        self.ax1.quiver(self.x_i[:,[0]],self.x_i[:,[1]],self.v_i[:,[0]],self.v_i[:,[1]])
+        
+
+    def animate(self, maxFrames, name):
+        """Method to animate the simulation
+            Args.:  maxFrames (int) - number of the Frames (and timesteps) for the animation
+                    name (string) - name of the saved .gif animation"""
+        simu = animation.FuncAnimation(self.fig, self.funcUpdate, frames = np.arange(0,maxFrames), interval = 25)
         simu.save(name+".gif", dpi = 80)
-
-
